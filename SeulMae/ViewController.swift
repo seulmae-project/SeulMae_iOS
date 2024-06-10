9971c0f2186487cb410bba3bce37297e1b74ce81
yonggipo <yonggipo@icloud.com>
2024-06-10 20:58:57 +0900

    Initial Commit

---
+++ SeulMae/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeulMae
+//
+//  Created by 조기열 on 6/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
