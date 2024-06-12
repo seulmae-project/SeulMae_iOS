d11d71c0f4aa9c563164c5244a907e86151688de
yonggipo <yonggipo@icloud.com>
2024-06-12 22:41:08 +0900

    feat: SignupCompletionVC layout 구성

--- SeulMae/Presentation/Authentication/Signup/SignupCompletion/View/SignupCompletionViewController.swift
+++ SeulMae/Presentation/Authentication/Signup/SignupCompletion/View/SignupCompletionViewController.swift
@@ -29,14 +29,54 @@ final class SignupCompletionViewController: UIViewController {
     private var viewModel: SignupCompletionViewModel!
     
     private var completionGuideLabel: UILabel = .callout(title: Text.completion)
-    private var guideLabel: UILabel = .title(Text.stepGuide)
+    private var stepGuideLabel: UILabel = .title(Text.stepGuide)
     private var nextStepButton: UIButton = .common(title: Text.nextStep)
    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureNavItem()
+        configureHierarchy()
     }
 
-
+    // MARK: - Nav Item
+    
+    private func configureNavItem() {
+        
+    }
+    
+    // MARK: - Hierarchy
+    
+    private func configureHierarchy() {
+        let subViews: [UIView] = [
+            completionGuideLabel, stepGuideLabel, nextStepButton
+        ]
+        subViews.forEach(view.addSubview)
+        
+        completionGuideLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(144)
+            make.top.equalTo(view.snp_topMargin).inset(132)
+            make.centerX.equalToSuperview()
+        }
+        
+        stepGuideLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(106)
+            make.top.equalTo(completionGuideLabel.snp.bottom).offset(8)
+            make.centerX.equalToSuperview()
+        }
+        
+//        nameFieldVStack.snp.makeConstraints { make in
+//            make.leading.equalToSuperview().inset(20)
+//            make.top.equalTo(genderFieldVStack.snp.bottom).offset(16)
+//            make.centerX.equalToSuperview()
+//        }
+        
+        nextStepButton.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(20)
+            make.bottom.equalTo(view.snp_bottomMargin).inset(24)
+            make.height.equalTo(56)
+            make.centerX.equalToSuperview()
+        }
+    }
 }
 
