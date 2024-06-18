1c9ac9544ff6b16c31c4d5516e9f499c5d58ac26
yonggipo <yonggipo@icloud.com>
2024-06-18 19:18:00 +0900

    feat: add sheetState enum

---
+++ SeulMae/Presentation/CommonUI/ModalViewController/SheetState.swift
@@ -0,0 +1,14 @@
+//
+//  SheetState.swift
+//  SeulMae
+//
+//  Created by 조기열 on 6/17/24.
+//
+
+import Foundation
+
+@objc enum SheetState: Int {
+    case closed
+    case preferred
+    case extended
+}
