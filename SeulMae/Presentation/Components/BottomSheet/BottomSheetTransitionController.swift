357b696e39282e5aebffa78786a29d59b9e90053
yonggipo <yonggipo@icloud.com>
2024-06-20 15:00:25 +0900

    feat: bottom sheet transition controller 정리

--- SeulMae/Presentation/Components/BottomSheet/BottomSheetTransitionController.swift
+++ SeulMae/Presentation/Components/BottomSheet/BottomSheetTransitionController.swift
@@ -7,9 +7,7 @@
 
 import UIKit
 
-private let MDCBottomSheetTransitionDuration: TimeInterval = 0.25
-
-class BottomSheetTransitionController: NSObject, UIViewControllerAnimatedTransitioning, UIViewControllerTransitioningDelegate {
+class BottomSheetTransitionController: NSObject {
     
     weak var trackingScrollView: UIScrollView?
     
@@ -27,73 +25,59 @@ class BottomSheetTransitionController: NSObject, UIViewControllerAnimatedTransit
     
     private var currentPresentationController: BottomSheetPresentationController?
     
+    private let bottomSheetTransitionDuration: TimeInterval = 0.25
+    
     // MARK: - Scrim Properties
-    
-    private var _scrimColor: UIColor?
-    
-    private var _isScrimAccessibilityElement = false
-    
-    private var _scrimAccessibilityLabel: String?
-    
-    private var _scrimAccessibilityHint: String?
-    
-    private var _scrimAccessibilityTraits: UIAccessibilityTraits = .button
-    
+        
     var scrimColor: UIColor? {
-        get { return _scrimColor }
-        set {
-            _scrimColor = newValue
-            currentPresentationController?.scrimColor = newValue
+        didSet {
+            currentPresentationController?.scrimColor = scrimColor
         }
     }
     
-    var isScrimAccessibilityElement: Bool {
-        get { return _isScrimAccessibilityElement }
-        set {
-            _isScrimAccessibilityElement = newValue
-            currentPresentationController?.isScrimAccessibilityElement = newValue
+    var isScrimAccessibilityElement: Bool = false {
+        didSet {
+            currentPresentationController?.isScrimAccessibilityElement = isScrimAccessibilityElement
         }
     }
     
     var scrimAccessibilityLabel: String? {
-        get { return _scrimAccessibilityLabel }
-        set {
-            _scrimAccessibilityLabel = newValue
-            currentPresentationController?.scrimAccessibilityLabel = newValue
+        didSet {
+            currentPresentationController?.scrimAccessibilityLabel = scrimAccessibilityHint
         }
     }
     
     var scrimAccessibilityHint: String? {
-        get { return _scrimAccessibilityHint }
-        set {
-            _scrimAccessibilityHint = newValue
-            currentPresentationController?.scrimAccessibilityHint = newValue
+        didSet {
+            currentPresentationController?.scrimAccessibilityHint = scrimAccessibilityHint
         }
     }
     
-    var scrimAccessibilityTraits: UIAccessibilityTraits {
-        get { return _scrimAccessibilityTraits }
-        set {
-            _scrimAccessibilityTraits = newValue
-            currentPresentationController?.scrimAccessibilityTraits = newValue
+    var scrimAccessibilityTraits: UIAccessibilityTraits = .button {
+        didSet {
+            currentPresentationController?.scrimAccessibilityTraits = scrimAccessibilityTraits
         }
     }
 }
 
-extension BottomSheetTransitionController {
+extension BottomSheetTransitionController: UIViewControllerAnimatedTransitioning, UIViewControllerTransitioningDelegate {
     
     // MARK: - UIViewControllerTransitioningDelegate
     
-    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+    func presentationController(
+        forPresented presented: UIViewController,
+        presenting: UIViewController?,
+        source: UIViewController
+    ) -> UIPresentationController? {
         let presentationController = BottomSheetPresentationController(presentedViewController: presented, presenting: presenting)
         presentationController.trackingScrollView = trackingScrollView
         presentationController.dismissOnBackgroundTap = dismissOnBackgroundTap
         presentationController.dismissOnDraggingDownSheet = dismissOnDraggingDownSheet
-        presentationController.scrimColor = _scrimColor
-        presentationController.scrimAccessibilityTraits = _scrimAccessibilityTraits
-        presentationController.isScrimAccessibilityElement = _isScrimAccessibilityElement
-        presentationController.scrimAccessibilityHint = _scrimAccessibilityHint
-        presentationController.scrimAccessibilityLabel = _scrimAccessibilityLabel
+        presentationController.scrimColor = scrimColor
+        presentationController.scrimAccessibilityTraits = scrimAccessibilityTraits
+        presentationController.isScrimAccessibilityElement = isScrimAccessibilityElement
+        presentationController.scrimAccessibilityHint = scrimAccessibilityHint
+        presentationController.scrimAccessibilityLabel = scrimAccessibilityLabel
         presentationController.preferredSheetHeight = preferredSheetHeight
         presentationController.adjustHeightForSafeAreaInsets = adjustHeightForSafeAreaInsets
         presentationController.ignoreKeyboardHeight = ignoreKeyboardHeight
@@ -101,29 +85,42 @@ extension BottomSheetTransitionController {
         return presentationController
     }
     
-    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+    func animationController(
+        forPresented presented: UIViewController,
+        presenting: UIViewController,
+        source: UIViewController
+    ) -> UIViewControllerAnimatedTransitioning? {
         return self
     }
     
-    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+    func animationController(
+        forDismissed dismissed: UIViewController
+    ) -> UIViewControllerAnimatedTransitioning? {
         return self
     }
     
     // MARK: - UIViewControllerAnimatedTransitioning
     
-    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-        return MDCBottomSheetTransitionDuration
+    func transitionDuration(
+        using transitionContext: UIViewControllerContextTransitioning?
+    ) -> TimeInterval {
+        return bottomSheetTransitionDuration
     }
     
-    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+    func animateTransition(
+        using transitionContext: UIViewControllerContextTransitioning
+    ) {
         guard let fromViewController = transitionContext.viewController(forKey: .from),
               let toViewController = transitionContext.viewController(forKey: .to) else {
             transitionContext.completeTransition(false)
             return
         }
         
-        let fromView = transitionContext.view(forKey: .from)
-        let toView = transitionContext.view(forKey: .to)
+        guard let fromView = transitionContext.view(forKey: .from),
+              let toView = transitionContext.view(forKey: .to) else {
+            transitionContext.completeTransition(false)
+            return
+        }
         
         let toPresentingViewController = toViewController.presentingViewController
         let presenting = (toPresentingViewController == fromViewController)
@@ -132,11 +129,9 @@ extension BottomSheetTransitionController {
         let animatingView = presenting ? toView : fromView
         
         let containerView = transitionContext.containerView
-//            transitionContext.completeTransition(false)
-//            return
         
         if presenting {
-            containerView.addSubview(toView!)
+            containerView.addSubview(toView)
         }
         
         let onscreenFrame = frameOfPresentedViewController(animatingViewController, in: containerView)
@@ -145,24 +140,33 @@ extension BottomSheetTransitionController {
         let initialFrame = presenting ? offscreenFrame : onscreenFrame
         let finalFrame = presenting ? onscreenFrame : offscreenFrame
         
-        animatingView?.frame = initialFrame
+        animatingView.frame = initialFrame
         
-        UIView.animate(withDuration: transitionDuration(using: transitionContext), delay: 0.0, options: [.allowUserInteraction, .beginFromCurrentState], animations: {
-            animatingView?.frame = finalFrame
+        UIView.animate(
+            withDuration: transitionDuration(using: transitionContext),
+            delay: 0.0,
+            options: [.allowUserInteraction, .beginFromCurrentState],
+            animations: {
+            animatingView.frame = finalFrame
         }) { (finished) in
             if !presenting {
-                fromView?.removeFromSuperview()
+                fromView.removeFromSuperview()
                 self.delegate?.didDismissBottomSheetTransitionController?(self)
             }
             transitionContext.completeTransition(true)
         }
     }
     
-    func frameOfPresentedViewController(_ presentedViewController: UIViewController, in containerView: UIView) -> CGRect {
+    
+    func frameOfPresentedViewController(
+        _ presentedViewController: UIViewController,
+        in containerView: UIView
+    ) -> CGRect {
         let containerSize = containerView.frame.size
         let preferredSize = presentedViewController.preferredContentSize
         
-        if preferredSize.width > 0 && preferredSize.width < containerSize.width {
+        if preferredSize.width > 0 &&
+            preferredSize.width < containerSize.width {
             let width = preferredSize.width
             let leftPad = (containerSize.width - width) / 2
             return CGRect(x: leftPad, y: 0, width: width, height: containerSize.height)
