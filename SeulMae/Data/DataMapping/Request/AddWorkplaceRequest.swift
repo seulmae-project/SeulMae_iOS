6b6f27cb26095731f3b5643fe38d6936e2cfbfda
yonggipo <yonggipo@icloud.com>
2024-07-02 03:34:21 +0900

    feat: add request for add work place

---
+++ SeulMae/Data/DataMapping/Request/AddWorkplaceRequest.swift
@@ -0,0 +1,15 @@
+//
+//  AddWorkplaceRequest.swift
+//  SeulMae
+//
+//  Created by 조기열 on 7/2/24.
+//
+
+import Foundation
+
+struct AddWorkplaceRequest {
+    var workplaceName: String
+    var mainAddress: String
+    var subAddress: String
+    var tel: String
+}
