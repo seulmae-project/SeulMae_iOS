64769cf2327c76d214252f9d85d4d6f6f6471477
yonggipo <yonggipo@icloud.com>
2024-07-02 03:56:19 +0900

    feat: add workplace apis

--- SeulMae/Domain/Interfaces/MainRepository.swift
+++ SeulMae/Domain/Interfaces/MainRepository.swift
@@ -10,5 +10,18 @@ import RxSwift
 
 protocol MainRepository {
     /// - Tag: Main
-
+    func fetchMemberList(_ workplaceID: String) -> Single<[Member]>
+    
+    /// - Tag: Workplace
+    func addWorkplace(_ request: AddWorkplaceRequest) -> Single<Bool>
+    func fetchWorkplaceList(_ keyword: String) -> Single<[Workplace]>
+    func fetchWorkplaceDetail(_ workplaceID: String) -> Single<Workplace>
+    func updateWorkplace(_ request: UpdateWorkplaceRequest) -> Single<Bool>
+    func deleteWorkplace(_ workplaceID: String) -> Single<Bool>
+    func submitApplication(_workplaceID: String) -> Single<Bool>    // body
+    func acceptApplication(workplaceApproveId: String, workplaceJoinHistoryId: String)
+    func denyApplication(workplaceApproveId: String, workplaceJoinHistoryId: String)
+    
+    /// - Tag: Common
+   
 }
