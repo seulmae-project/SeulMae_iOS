2168e48b616bea1a2bf671fd3f3564ea8e838157
용기포 <yonggipo@icloud.com>
2024-09-01 20:12:45 +0900

    feat: configure parent announce vc

---
+++ SeulMae/Presentation/Announce/Announce/VewModel/AnnounceViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AnnounceViewModel.swift
+//  SeulMae
+//
+//  Created by 조기열 on 8/31/24.
+//
+
+import Foundation
---
+++ SeulMae/Presentation/Announce/Announce/View/AnnounceViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AnnounceViewController.swift
+//  SeulMae
+//
+//  Created by 조기열 on 8/31/24.
+//
+
+import Foundation
+
+final class AnnounceViewController {
+    
+}
