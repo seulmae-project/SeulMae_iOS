fc9236fe0709c807a7364da3c897ce7329c99c1e
용기포 <yonggipo@icloud.com>
2024-09-13 21:13:17 +0900

    feat: add monthlyAttendnaceSummary entitiy & dto

---
+++ SeulMae/Domain/Entities/MonthlyAttenanceSummary.swift
@@ -0,0 +1,43 @@
+//
+//  MonthlyAttenanceSummary.swift
+//  SeulMae
+//
+//  Created by 조기열 on 9/13/24.
+//
+
+import Foundation
+
+struct MonthlyAttenanceSummary {
+    let monthlyWage: Double
+    let applyStartDate: Date
+    let applyEndDate: Date
+    let baseWage: Double
+    let monthlyWorkTime: Int
+}
+
+struct MonthlyAttenanceSummaryDTO: ModelType {
+    let monthlyWage: Double
+    let applyStartDate: Date
+    let applyEndDate: Date
+    let baseWage: Double
+    let monthlyWorkTime: Int?
+}
+
+extension BaseResponseDTO<[MonthlyAttenanceSummaryDTO]> {
+    func toDomain() -> [MonthlyAttenanceSummary] {
+        return data?.map { $0.toDomain() } ?? []
+    }
+}
+
+extension MonthlyAttenanceSummaryDTO {
+    func toDomain() -> MonthlyAttenanceSummary {
+        return .init(
+            monthlyWage: self.monthlyWage,
+            applyStartDate: self.applyStartDate,
+            applyEndDate: self.applyEndDate,
+            baseWage: self.baseWage,
+            monthlyWorkTime: self.monthlyWorkTime ?? 0
+        )
+    }
+}
+
