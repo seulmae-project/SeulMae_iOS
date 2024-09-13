183f2e22de3d42011735f363df8519ce7c0c7a05
용기포 <yonggipo@icloud.com>
2024-09-13 21:06:37 +0900

    feat: add attendanceHistory entity & dto

--- SeulMae/Domain/Entities/AttendanceHistory.swift
+++ SeulMae/Domain/Entities/AttendanceHistory.swift
@@ -8,5 +8,61 @@
 import Foundation
 
 struct AttendanceHistory: Identifiable {
+    let id: Int // idAttendanceRequestHistory
+    let workDate: Date
+    let workStartTime: Date
+    let workEndTime: Date
+    let totalWorkTime: Double
+    let wage: Double
+    let isRequestApprove: Bool
+    let isManagerCheck: Bool
+}
+
+struct AttendanceHistoryDTO: ModelType {
     let id: Int
+    let workDate: Date
+    let workStartTime: Date
+    let workEndTime: Date
+    let totalWorkTime: Double
+    let wage: Double
+    let isRequestApprove: Bool
+    let isManagerCheck: Bool
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "idAttendanceRequestHistory"
+        case workDate
+        case workStartTime
+        case workEndTime
+        case totalWorkTime
+        case wage
+        case isRequestApprove
+        case isManagerCheck
+    }
+}
+
+extension BaseResponseDTO<AttendanceHistoryDTO> {
+    func toDomain() -> AttendanceHistory? {
+        return data?.toDomain()
+    }
+}
+
+extension BaseResponseDTO<[AttendanceHistoryDTO]> {
+    func toDomain() -> [AttendanceHistory] {
+        return data?.map { $0.toDomain() } ?? []
+    }
+}
+
+extension AttendanceHistoryDTO {
+    func toDomain() -> AttendanceHistory {
+        return AttendanceHistory(
+            id: self.id,
+            workDate: self.workDate,
+            workStartTime: self.workDate,
+            workEndTime: self.workDate,
+            totalWorkTime: self.totalWorkTime,
+            wage: self.wage,
+            isRequestApprove: self.isRequestApprove,
+            isManagerCheck: self.isManagerCheck
+        )
+    }
 }
